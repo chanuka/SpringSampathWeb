533e5be25bf6d09e13d6ec14a38185fbfb72a129
chanuka <chanuka@DESKTOP-1A5V6K9>
2016-12-21 07:06:42 +0530

    commit by chanu

---
+++ src/main/java/com/epic/springsampathweb/util/common/AuditBean.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.epic.springsampathweb.util.common;
+
+import java.util.Date;
+
+/**
+ *
+ * @author chanuka
+ */
+public class AuditBean {
+
+//    private long systemauditid;
+    private String userrole;
+    private String section;
+    private String page;
+    private String task;
+    private String ip;
+    private String oldvalue;
+    private String newvalue;
+    private String lastupdateduser;
+    private Date lastupdatedtime;
+    private Date createtime;
+    private String description;
+
+    public String getUserrole() {
+        return userrole;
+    }
+
+    public void setUserrole(String userrole) {
+        this.userrole = userrole;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getPage() {
+        return page;
+    }
+
+    public void setPage(String page) {
+        this.page = page;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getOldvalue() {
+        return oldvalue;
+    }
+
+    public void setOldvalue(String oldvalue) {
+        this.oldvalue = oldvalue;
+    }
+
+    public String getNewvalue() {
+        return newvalue;
+    }
+
+    public void setNewvalue(String newvalue) {
+        this.newvalue = newvalue;
+    }
+
+    public String getLastupdateduser() {
+        return lastupdateduser;
+    }
+
+    public void setLastupdateduser(String lastupdateduser) {
+        this.lastupdateduser = lastupdateduser;
+    }
+
+    public Date getLastupdatedtime() {
+        return lastupdatedtime;
+    }
+
+    public void setLastupdatedtime(Date lastupdatedtime) {
+        this.lastupdatedtime = lastupdatedtime;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+    
+
+}
